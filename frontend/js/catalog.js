b040ea3e16cf68d2e78623720feaf3ee2c6c618a
Lucyy_ <138951685+Greyln@users.noreply.github.com>
2024-11-13 18:02:28 -0300

    Update catalog.js

--- frontend/js/catalog.js
+++ frontend/js/catalog.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-
-	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE
 	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-	// FILTRANDO PRODUCTOS  ============================================
-
+	// FILTRANDO PRODUCTOS
 	$('.category_item').click(function(){
 		var catProduct = $(this).attr('category');
 		console.log(catProduct);
@@ -13,21 +11,20 @@ $(document).ready(function(){
 		$('.category_item').removeClass('ct_item-active');
 		$(this).addClass('ct_item-active');
 
-		// OCULTANDO PRODUCTOS =========================
+		// OCULTANDO PRODUCTOS
 		$('.product-item').css('transform', 'scale(0)');
 		function hideProduct(){
 			$('.product-item').hide();
 		} setTimeout(hideProduct,400);
 
-		// MOSTRANDO PRODUCTOS =========================
+		// MOSTRANDO PRODUCTOS
 		function showProduct(){
 			$('.product-item[category="'+catProduct+'"]').show();
 			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
 		} setTimeout(showProduct,400);
 	});
 
-	// MOSTRANDO TODOS LOS PRODUCTOS =======================
-
+	// MOSTRANDO TODOS LOS PRODUCTOS
 	$('.category_item[category="all"]').click(function(){
 		function showAll(){
 			$('.product-item').show();
